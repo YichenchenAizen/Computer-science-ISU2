0636659f79d2c5b4bce2daf5c3feb4d3071bc93d
YichenchenAizen <349352880@gapps.yrdsb.ca>
2026-01-15 17:13:00 -0500

    Add files via upload

--- hogyoku/src/Main.java
+++ hogyoku/src/Main.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
-    }
-}
+        String[] questionsA = new String[10];
+        String[] answersA = new String[10];
+        String[] questionsB = new String[10];
+        String[] answersB = new String[1];
+        int countA = 10;
+        int countB = 10;
+        String ADMIN_PASSWORD = "admin12345";
+        System.out.println("Welcome to the road rules G1 practice test!!!");
+        while (true) {
+            System.out.println("Main menu:");
+            System.out.println("1. Write a G1 test");
+            System.out.println("2. Login for Admin");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+            String choice = sc.nextLine();
+            if (choice.equals("1")){
+
+            }
+            }
+        }
